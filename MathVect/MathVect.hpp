4aec2edb876acf194ebe462e313a17bdeb86b156
Fabian Reiner <fabian.reiner@hotmail.com>
2022-05-31 11:12:55 +0200

    math vectors nearly done

--- MathVect/MathVect.hpp
+++ MathVect/MathVect.hpp
@@ -2,21 +2,174 @@
 #define _MATH_VECTORS_HPP_
 
   #include <vector>
-  #include <numeric>
   #include <concepts>
+  #include <ostream>
+  #include <stdexcept>
+  #include <math.h>
 
-  template<typename T>
-    concept arithmetic = std::integral<T> or std::floating_point<T>;
+  template <typename T>
+  concept Number = std::integral<T> || std::floating_point<T>;
 
-  template<typename T> 
-    requires arithmetic<T>
-  class MathVect : public std::vector<T>
+  template <Number num>
+  class MathVect : public std::vector<num>
   {
-    private:
-      /* data */
     public:
-      MathVect(/* args */);
-      ~MathVect();
+      using std::vector<num>::vector;
+      
+      num getLength() const
+      {
+        return sqrt(*this * *this);
+      }
+
+      friend MathVect operator+(const MathVect& lhs, const MathVect& rhs)
+      {
+        MathVect<num> vect;
+
+        size_t lhs_length = lhs.size();
+        if(lhs_length == rhs.size() && lhs_length > 0)
+        {
+          for (size_t i = 0; i < lhs_length; i++)
+          {
+            vect.push_back(lhs.at(i) + rhs.at(i));
+          }
+        }
+        else
+        {
+          throw std::runtime_error("[calculation failed]: Vectors have an invalid size!");
+        }
+
+        return vect;
+      }
+
+      friend MathVect operator-(const MathVect& lhs, const MathVect& rhs)
+      {
+        MathVect<num> vect;
+
+        size_t lhs_length = lhs.size();
+        if(lhs_length == rhs.size() && lhs_length > 0)
+        {
+          for (size_t i = 0; i < lhs_length; i++)
+          {
+            vect.push_back(lhs.at(i) - rhs.at(i));
+          }
+        }
+        else
+        {
+          throw std::runtime_error("[calculation failed]: Vectors have an invalid size!");
+        }
+
+        return vect;
+      }
+
+      friend num operator*(const MathVect& lhs, const MathVect& rhs)
+      {
+        MathVect<num> vect;
+        num sum = 0;
+
+        size_t lhs_length = lhs.size();
+        if(lhs_length == rhs.size() && lhs_length > 0)
+        {
+          for (size_t i = 0; i < lhs_length; i++)
+          {
+            vect.push_back(lhs.at(i) * rhs.at(i));
+          }
+
+          for(auto& value : vect)
+          {
+            sum += value;
+          }
+        }
+        else
+        {
+          throw std::runtime_error("[calculation failed]: Vectors have an invalid size!");
+        }
+
+        return sum;
+      }
+
+      MathVect& operator+=(const MathVect& rhs)
+      {
+        size_t this_length = this->size();
+        if(this_length == rhs.size() && this_length > 0)
+        {
+          for (size_t i = 0; i < this_length; i++)
+          {
+            this->at(i) += rhs.at(i);
+          }
+        }
+        else
+        {
+          throw std::runtime_error("[calculation failed]: Vectors have an invalid size!");
+        }
+
+        return *this;
+      }
+
+      MathVect& operator-=(const MathVect& rhs)
+      {
+        size_t this_length = this->size();
+        if(this_length == rhs.size() && this_length > 0)
+        {
+          for (size_t i = 0; i < this_length; i++)
+          {
+            this->at(i) -= rhs.at(i);
+          }
+        }
+        else
+        {
+          throw std::runtime_error("[calculation failed]: Vectors have an invalid size!");
+        }
+
+        return *this;
+      }
+
+      template<Number scalar_num>
+      friend MathVect operator*(const MathVect& lhs, const scalar_num& scalar)
+      {
+        MathVect<num> vect;
+
+        size_t lhs_length = lhs.size();
+        for (size_t i = 0; i < lhs_length; i++)
+        {
+          vect.push_back(lhs.at(i) * scalar);
+        }
+
+        return vect;
+      }
+
+      template<Number scalar_num>
+      friend MathVect operator*(const scalar_num& scalar, const MathVect& lhs)
+      {
+        return (lhs * scalar);
+      }
+
+      template<Number scalar_num>
+      MathVect& operator*=(const scalar_num& scalar)
+      {
+        size_t this_length = this->size();
+        for (size_t i = 0; i < this_length; i++)
+        {
+          this->at(i) *= scalar;
+        }
+
+        return *this;
+      }
+    
+      friend std::ostream& operator<<(std::ostream& out, const MathVect& rhs)
+      {
+        out << "{ ";
+
+        auto rhs_iter = rhs.begin();
+        out << std::to_string(*(rhs_iter++));
+
+        while (rhs_iter != rhs.end())
+        {
+          out << ", " << std::to_string(*(rhs_iter++));
+        }
+        out << " }";
+
+        return out;
+      }
   };
 
 
--- main.cpp
+++ main.cpp
@@ -4,6 +4,14 @@
 
 int main()
 {
-  std::cout << "Hello World!" << std::endl;
+  MathVect<float> myVect1{ 5, 3 };
+  MathVect<float> myVect2{ 6, 7 };
+
+  std::cout << "Scalar product Vect1 & Vect2: " << myVect1 * myVect2 << std::endl;
+  std::cout << std::endl;
+  std::cout << "Length Vect1: " << myVect1.getLength() << std::endl;
+  std::cout << std::endl;
+  std::cout << "Vect1: " << myVect1 << std::endl;
+  std::cout << "Vect2: " << myVect2 << std::endl;
   return 0;
 }
